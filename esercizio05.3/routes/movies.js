538010391fe4b605a96e3079daf9bd68dca95786
gioele-bra <brancaleoni.gioele@iisgalvanimi.edu.it>
2020-11-06 15:19:37 +0000

    Esercizio 05.3 Parte 2

--- esercizio05.3/routes/movies.js
+++ esercizio05.3/routes/movies.js
@@ -20,4 +20,42 @@ router.get('/movie_from_title/:title', function (req, res, next) {
     });
 });
 
+router.get('/list/:num', function (req, res, next) {
+    let num = parseInt(req.params.num);
+    const uri = 'mongodb+srv://gioele-bra:hGSdfpfQKAke3STS@cluster0.dbkxm.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // perform actions on the collection object
+        collection.find().limit(num).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else { 
+                res.send(result);
+            }
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+    
+});
+
+router.get('/year/:num', function (req, res, next) {
+    let num = parseInt(req.params.num);
+    const uri = 'mongodb+srv://gioele-bra:hGSdfpfQKAke3STS@cluster0.dbkxm.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
+        // perform actions on the collection object
+        collection.find('{year:{'+num+'}}').toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else { 
+                res.send(result);
+            }
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+    
+});
+
 module.exports = router;
