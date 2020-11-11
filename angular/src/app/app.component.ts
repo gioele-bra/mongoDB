97ba053104964ef175455276fae1ca38bb730768
gioele-bra <brancaleoni.gioele@iisgalvanimi.edu.it>
2020-11-11 13:52:14 +0000

    Esercizio 05.3.2 - Parte 1

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer} from '@angular/platform-browser';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'mflix-visualizer';
+  results : Object[];
+  obs : Observable<object>;
+  constructor(private http : HttpClient, private sanitizer: DomSanitizer){}
+
+  load10Movies()
+  {
+    this.obs = this.http.get("https://3000-ae4061bd-74d5-4d12-8735-9c0cc7bc0671.ws-eu01.gitpod.io/users");
+    this.obs.subscribe(this.getData);
+  }
+
+  getData = (data) => {
+    this.results = data;
+  }
+
+  photoURL(urltoSanitize) {
+    console.log(urltoSanitize);
+    return this.sanitizer.bypassSecurityTrustUrl(urltoSanitize);
+    }
+
 }
